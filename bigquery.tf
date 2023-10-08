cfc19a16d1ab1f6e28953b2808e3062b4bca7be4
Taku Kotegawa <tkotegawa@stnet.co.jp>
2023-10-08 16:53:05 +0900

    bigquery setting

---
+++ bigquery.tf
@@ -0,0 +1,49 @@
+# ---------------------------------------------------------
+# データセット
+# ---------------------------------------------------------
+
+resource "google_bigquery_dataset" "dataset" {
+    dataset_id                      = "example_dataset"
+    friendly_name                   = "test"
+    description                     = "This is a test description"
+    default_table_expiration_ms     = 5184000000
+    default_partition_expiration_ms = 5184000000
+    location                        = "asia-northeast1"
+  }
+
+# ---------------------------------------------------------
+# テーブル
+# ---------------------------------------------------------
+resource "google_bigquery_table" "t_users" {
+  dataset_id          = google_bigquery_dataset.dataset.dataset_id
+  table_id            = "t_users"
+  deletion_protection = false
+
+  schema = file("./schema/t_users.json")
+
+}
+
+# ---------------------------------------------------------
+# プロシージャ
+# ---------------------------------------------------------
+
+
+// マート作成
+
+resource "google_bigquery_routine" "read_t_users" {
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  routine_id     = "read_t_users"
+  routine_type = "PROCEDURE"
+  language = "SQL"
+  definition_body = file("./procedure/read_t_users.txt")
+}
+
+resource "google_bigquery_routine" "read_t_users2" {
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  routine_id     = "read_t_users2"
+  routine_type = "PROCEDURE"
+  language = "SQL"
+  definition_body = file("./procedure/read_t_users.txt")
+}
+
+
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+    project = "our-truth-399407"
+    region  = "asia-northeast1"
+  }
+  
