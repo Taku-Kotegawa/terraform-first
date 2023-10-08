b79239b0f60b785047c5c198e26e65daf25d5a32
Taku Kotegawa <tkotegawa@stnet.co.jp>
2023-10-08 16:57:26 +0900

    フォーマット修正

--- bigquery.tf
+++ bigquery.tf
@@ -3,13 +3,13 @@
 # ---------------------------------------------------------
 
 resource "google_bigquery_dataset" "dataset" {
-    dataset_id                      = "example_dataset"
-    friendly_name                   = "test"
-    description                     = "This is a test description"
-    default_table_expiration_ms     = 5184000000
-    default_partition_expiration_ms = 5184000000
-    location                        = "asia-northeast1"
-  }
+  dataset_id                      = "example_dataset"
+  friendly_name                   = "test"
+  description                     = "This is a test description"
+  default_table_expiration_ms     = 5184000000
+  default_partition_expiration_ms = 5184000000
+  location                        = "asia-northeast1"
+}
 
 # ---------------------------------------------------------
 # テーブル
@@ -31,18 +31,18 @@ resource "google_bigquery_table" "t_users" {
 // マート更新
 
 resource "google_bigquery_routine" "read_t_users" {
-  dataset_id = google_bigquery_dataset.dataset.dataset_id
-  routine_id     = "read_t_users"
-  routine_type = "PROCEDURE"
-  language = "SQL"
+  dataset_id      = google_bigquery_dataset.dataset.dataset_id
+  routine_id      = "read_t_users"
+  routine_type    = "PROCEDURE"
+  language        = "SQL"
   definition_body = file("./procedure/read_t_users.txt")
 }
 
 resource "google_bigquery_routine" "read_t_users2" {
-  dataset_id = google_bigquery_dataset.dataset.dataset_id
-  routine_id     = "read_t_users2"
-  routine_type = "PROCEDURE"
-  language = "SQL"
+  dataset_id      = google_bigquery_dataset.dataset.dataset_id
+  routine_id      = "read_t_users2"
+  routine_type    = "PROCEDURE"
+  language        = "SQL"
   definition_body = file("./procedure/read_t_users.txt")
 }
 
